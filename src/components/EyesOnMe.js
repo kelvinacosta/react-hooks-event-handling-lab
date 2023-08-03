8d1e205560acfe82e0e85874d834efc64fd832b8
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-08-02 20:36:34 -0400

    complete events lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+
+function EyesOnMe(){
+    
+    //Create an anonymous function
+    const handleFocus = () => {
+        console.log('Good!')
+    }
+    //Function declaration 
+    function handleBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+    
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
